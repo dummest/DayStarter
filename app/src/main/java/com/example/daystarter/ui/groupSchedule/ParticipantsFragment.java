85a7a4682a70421678297adb6c5fcae215997186
dummest <rhqemddlakst@gmail.com>
2022-03-30 15:41:12 +0900

    making group schedule tab

---
+++ app/src/main/java/com/example/daystarter/ui/groupSchedule/ParticipantsFragment.java
@@ -0,0 +1,22 @@
+package com.example.daystarter.ui.groupSchedule;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.daystarter.R;
+
+
+public class ParticipantsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_participants, container, false);
+    }
+}
