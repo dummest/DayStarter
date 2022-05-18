93b61efa21c5bc50cf7ee2d23789a0c701cf4ee8
oh-seungmin <gkahsl13@naver.com>
2022-05-18 16:02:31 +0900

    weekday(weather_no icon)

--- app/src/main/java/com/example/daystarter/ui/weather/WeatherAdapter.java
+++ app/src/main/java/com/example/daystarter/ui/weather/WeatherAdapter.java
@@ -1,19 +1,26 @@
 package com.example.daystarter.ui.weather;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.daystarter.R;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class WeatherAdapter extends RecyclerView.Adapter{
-    ArrayList<weatherData> item;
+    ArrayList<WeatherWeekData> ArrayData;
+
+    public WeatherAdapter(ArrayList<WeatherWeekData> data, FragmentActivity activity){
+        this.ArrayData = data;
+    }
 
     @NonNull
     @Override
@@ -25,28 +32,57 @@ public class WeatherAdapter extends RecyclerView.Adapter{
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Log.d("onBindViewHolder", "onBindViewHolder: ");
         WeatherViewHolder wv =(WeatherViewHolder)holder;
+        WeatherWeekData weatherdata =ArrayData.get(position);
 
-        weatherData data =item.get(position);
+        wv.weather_week.setText(weatherdata.getNameDate());
+        wv.highest_temperature.setText(String.format(Locale.getDefault(),"%.0f°C",weatherdata.MaxTemp));
+        wv.lowest_temperature.setText(String.format(Locale.getDefault(),"%.0f°C",weatherdata.MinTemp));
 
+
+         //weather을 가져와 적절한 아이콘 집어넣기
+        if(weatherdata.getWeather().equals("fog")){
+            wv.weather_img.setText("안개");
+        }
+        else if (weatherdata.getWeather().equals("clouds")){
+            wv.weather_img.setText("구름");
+        }
+        else if (weatherdata.getWeather().equals("few clouds")){
+            wv.weather_img.setText("구름 조금");
+        }
+        else if (weatherdata.getWeather().equals("scattered clouds")){
+            wv.weather_img.setText("구름 낌");
+        }
+        else if (weatherdata.getWeather().equals("broken clouds")){
+            wv.weather_img.setText("구름 많음");
+        }
+        else if (weatherdata.getWeather().equals("overcast clouds")){
+            wv.weather_img.setText("구름 많음");
+        }
+        else if(weatherdata.getWeather().equals("clear sky")){
+            wv.weather_img.setText("맑음");
+        }
+        else
+            wv.weather_img.setText("");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return ArrayData.size();
     }
 
     class WeatherViewHolder extends RecyclerView.ViewHolder{
 
-        TextView weather_day,weather_week,highest_temperature,lowest_temperature,humidity;
+        TextView weather_week,highest_temperature,lowest_temperature,weather_img;
 
         public WeatherViewHolder(@NonNull View itemView) {
             super(itemView);
-            weather_day=itemView.findViewById(R.id.weather_day);
             weather_week=itemView.findViewById(R.id.weather_week);
             highest_temperature =itemView.findViewById(R.id.highest_temperature);
             lowest_temperature =itemView.findViewById(R.id.lowest_temperature);
-            humidity=itemView.findViewById(R.id.humidity);
+            weather_img =itemView.findViewById(R.id.weather_img);
         }
     }
+
 }
--- app/src/main/java/com/example/daystarter/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/example/daystarter/ui/weather/WeatherFragment.java
@@ -1,9 +1,12 @@
 package com.example.daystarter.ui.weather;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,26 +20,46 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.bumptech.glide.Glide;
 import com.example.daystarter.R;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import kotlin.jvm.internal.Intrinsics;
 
 public class WeatherFragment extends Fragment {
     //@BindView(R.id.tv_name) TextView tv_name;
     //@BindView(R.id.tv_country) TextView tv_country;
+    @BindView(R.id.weather_recyclerview) RecyclerView recyclerView;
     Context context;
 
     TextView tv_name, tv_country;
     ImageView iv_weather;
     TextView tv_temp, tv_main, tv_description;
     TextView tv_wind, tv_cloud, tv_humidity;
+    ArrayList<WeatherWeekData> weatherWeekData= new ArrayList<>();
+    WeatherAdapter weatherAdapter;
+    LocationManager locationManager =null;
+    double lat=0;
+    double lng=0;
 
     String strUrl = "https://api.openweathermap.org/data/2.5/weather";  //통신할 URL
     NetworkTask networkTask = null;
@@ -46,7 +69,11 @@ public class WeatherFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v;
         v = inflater.inflate(R.layout.fragment_weather, container, false);
-        //ButterKnife.bind(this,v);
+        ButterKnife.bind(this,v);
+        weatherAdapter = new WeatherAdapter(weatherWeekData,getActivity());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(weatherAdapter);
+
 
         tv_name = (TextView) v.findViewById(R.id.tv_name);
         tv_country = (TextView) v.findViewById(R.id.tv_country);
@@ -57,7 +84,20 @@ public class WeatherFragment extends Fragment {
         tv_wind = (TextView) v.findViewById(R.id.tv_wind);
         tv_cloud = (TextView) v.findViewById(R.id.tv_cloud);
         tv_humidity = (TextView) v.findViewById(R.id.tv_humidity);
-        requestNetwork();
+
+        /*
+         현재 위치하고 있는 위도 경도를 가져오려했지만 bug로 인해 잠시 대기
+        locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        @SuppressLint("MissingPermission") Location current = locationManager. getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //위도 경도
+        lat =current.getLongitude();
+        //경도
+        lng = current.getLatitude();
+        */
+
+        //일주일 날씨
+        getLocation();
+        //주별 날씨
         return v;
     }
 
@@ -65,8 +105,8 @@ public class WeatherFragment extends Fragment {
     /* NetworkTask 를 요청하기 위한 메소드 */
     private void requestNetwork() {
         ContentValues values = new ContentValues();
-        //인천 날씨(바꾸고 싶으면 여기서 교채하면된다.)
-        values.put("q", "Incheon");
+        //기본값 =서울날씨(바꾸고 싶으면 여기서 교채하면된다.)
+        values.put("q", "Seoul");
         values.put("appid", getString(R.string.weather_app_id));
 
         networkTask = new NetworkTask(context, strUrl, values);
@@ -149,7 +189,6 @@ public class WeatherFragment extends Fragment {
 
     }  //NetworkTask End
 
-
     /* 통신하여 받아온 날씨 데이터를 통해 UI 업데이트 메소드 */
     private void setWeatherData(weatherData model) {
         Log.d("Weather", "setWeatherData");
@@ -216,6 +255,68 @@ public class WeatherFragment extends Fragment {
             return "맑음";
         return "";
     }
+
+    private void getLocation(){
+        Log.d("getLocation", "getLocation: ");
+
+        requestNetwork();
+        // android-networking 위도 경도에 따라 지역 달라짐(현재는 서울)
+        AndroidNetworking.get("https://api.openweathermap.org/data/2.5/onecall?lat=37.5683&lon=126.977&exclude=current,minutely,hourly,alerts&units=metric&appid=7e818b3bfae91bb6fcbe3d382b6c3448")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Intrinsics.checkNotNullParameter(response, "response");
+                        Log.d("onResponse", "onResponse:");
+                        try {
+                            //daily 기준으로 파싱
+                            JSONArray jsonArray = response.getJSONArray("daily");
+                            for(int i=0 ;i<jsonArray.length(); i++) {
+                                WeatherWeekData data = new WeatherWeekData();
+                                JSONObject objectList = jsonArray.getJSONObject(i);
+                                //daily 안에 있는 temp에 있는 내용 파싱
+                                JSONObject jsonObjectOne = objectList.getJSONObject("temp");
+                                JSONArray jsonArrayOne = objectList.getJSONArray("weather");
+                                JSONObject jsonObjectTwo = jsonArrayOne.getJSONObject(0);
+                                //날짜가져오기 xx일
+                                long Day = objectList.optLong("dt");
+                                SimpleDateFormat formatDate = new java.text.SimpleDateFormat("dd일");
+                                String readableDate = formatDate.format(new java.util.Date(Day * (long)1000));
+                                //요일 가져오기 ex) 월요일
+                                long weekday = objectList.optLong("dt");
+                                SimpleDateFormat format = new SimpleDateFormat("EEEE");
+                                String readableDay = format.format(new Date(weekday * (long)1000));
+                                
+                                //순서대로 날짜+요일, 날씨 상태,최저온도,최고온도
+                                data.setNameDate(readableDate + ' ' + readableDay);
+                                data.setWeather(jsonObjectTwo.getString("description"));
+                                data.setMinTemp(jsonObjectOne.getDouble("min"));
+                                data.setMaxTemp(jsonObjectOne.getDouble("max"));
+
+                                Log.d("weatherData" ,"addData: ");
+                                weatherWeekData.add(data);
+                            }
+                            weatherAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Toast.makeText(getActivity(), "인터넷에 연결하지 못하였습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+    private void setWeekWeatherData(weatherData model){
+        Log.d("Weather", "setWeatherData");
+        tv_name.setText(model.getName());
+        tv_country.setText(model.getCountry());
+
+    }
+
 
 
 
